63501708485fbbe2e40857bd3f18c3a0104dfcea
Alex Blair <bclan11@gmail.com>
2022-06-12 13:13:11 -0700

    append with fs and main function

--- wordCount.js
+++ wordCount.js
@@ -1,8 +1,5 @@
 //Assuming that all spaces are separating words without punctuation or non letter characters (except for apostrophes/contractions). Assuming same formatting as example in email including descending sort, lowercase, and line spacing. Assuming this function doesn't need a guard against running repeatedly which starts to grow with numbers as keys as well. Using vanilla javascript.
 
-const text = fs.readFileSync('./text-to-count.txt', 'utf-8');
-const wordArr = text.split(' ');
-
 function countWords(array) {
   // Using forEach to iterate over words in the text array and convert them into lowercase for evaluation. If word is key in wordCount object, then increment value by 1 otherwise make a {key: value} pair {word: 1} in wordCount object
   const wordsCount = {};
@@ -20,5 +17,21 @@ function sortWordsByFrequency(object) {
   console.log('sortedWords :>> ', sortedWords);
   return sortedWords;
 }
-const countObj = countWords(wordArr);
-sortWordsByFrequency(countObj);
+
+function appendCountsToFile(array, filePath) {
+  array.forEach((word) => {
+    const wordCount = `${word[0]} ${word[1]}`;
+    fs.appendFileSync(filePath, '\n\n' + wordCount, (error) => {
+      if (error) throw error;
+    });
+  });
+}
+
+function countWordsAndAppendTotals(filePath) {
+  const text = fs.readFileSync('./text-to-count.txt', 'utf-8');
+  const wordArr = text.split(' ');
+
+  const countObj = countWords(wordArr);
+  const sortedWords = sortWordsByFrequency(countObj);
+  appendCountsToFile(sortedWords, filePath);
+}
